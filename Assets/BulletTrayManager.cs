337348da25f092ce609f65816ab0ef12ef3a1365
Mahadi Hassan <65780684+hrud09@users.noreply.github.com>
2023-09-18 00:02:12 +0600

    Bugs Tray

--- Assets/BulletTrayManager.cs
+++ Assets/BulletTrayManager.cs
@@ -5,37 +5,47 @@ using DG.Tweening;
 
 public class BulletTrayManager : MonoBehaviour
 {
-    public List<BulletTray> bulletTrays;
+    public GameObject bulletTrayPrefab;
+    public List<BulletTray> SpawnedBulletTrays;
     public BulletTray currentFreeBulletTray;
     public Transform[] bulletTrayPositions;
-    public List<BulletTray> freeTrays;
     public BulletSpawnManager bulletSpawnManager;
     // Update is called once per frame
     void Awake()
     {
-        freeTrays = bulletTrays;
-        currentFreeBulletTray = freeTrays[0];
         bulletSpawnManager.bulletCapacityLeft = currentFreeBulletTray.bulletCapacity;
     }
 
     public void TrackFreeBulletTrays(BulletTray filledTray)
     {
         currentFreeBulletTray = null;
-        freeTrays.Remove(filledTray);
-        if (freeTrays.Count > 0) {
-            filledTray.transform.DOMove(bulletTrayPositions[1].position, 2f).SetDelay(1).OnComplete(() => {
 
-                GameObject tray = freeTrays[0].gameObject;
-                tray.transform.GetChild(0).localScale = Vector3.zero;
-                tray.SetActive(true);
-                tray.transform.GetChild(0).DOScale(Vector3.one * 1.6f, 0.3f).OnComplete(() => {
-                    currentFreeBulletTray = freeTrays[0];
-                    bulletSpawnManager.bulletCapacityLeft = currentFreeBulletTray.bulletCapacity;
-                    bulletSpawnManager.unpackAreaManager.CheckForUnpacking();
-                });
+        filledTray.transform.DOMove(bulletTrayPositions[1].position, 2f).SetDelay(1).OnComplete(() =>
+        {
+
+            GameObject tray = Instantiate(bulletTrayPrefab, bulletTrayPositions[0]);
+            currentFreeBulletTray = tray.GetComponent<BulletTray>();
+            SpawnedBulletTrays.Add(currentFreeBulletTray);
+            tray.transform.GetChild(0).localScale = Vector3.zero;
+            tray.SetActive(true);
+            tray.transform.GetChild(0).DOScale(Vector3.one * 1.6f, 0.3f).OnComplete(() =>
+            {
+                bulletSpawnManager.bulletCapacityLeft = currentFreeBulletTray.bulletCapacity;
+                bulletSpawnManager.unpackAreaManager.CheckForUnpacking();
             });
+        });
+    }
 
+    void Reposition()
+    {
+        if (SpawnedBulletTrays.Count > 0)
+        {
+            for (int i = 0; i < disposedPackages.Count; i++)
+            {
+                float stackHeight = Mathf.Floor(i / disposPoses.Length);
+                Vector3 newPosition = disposPoses[i % disposPoses.Length].position + Vector3.up * spawnedItemGap * stackHeight;
+                disposedPackages[i].transform.position = newPosition;
+            }
         }
-
     }
 }
